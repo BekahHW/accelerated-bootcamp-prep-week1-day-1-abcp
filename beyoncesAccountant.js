89282023b75797f28957b570c83d9f57e665ccb4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-13 17:30:50 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -4,6 +4,15 @@ console.log("You have earned " + beyoncesMoney)
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
+function beyoncesMoney(){
+  var of= (500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000)
+  
+  var livePerformances=()
+  var musicSales=()
+  
+  
+}
+
 
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
